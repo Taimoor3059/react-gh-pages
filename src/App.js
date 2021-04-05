7c2eb9d27bd6f5177549440effa1847cee4f9523
Taimoor Tahir <taimoorhtahir@gmail.com>
2021-04-05 16:34:28 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState, } from 'react';
+import { HiLightBulb } from "react-icons/hi";
+import { GoLightBulb } from "react-icons/go";
 function App() {
+
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+
+  const [light, lightValue] = useState(false);
+
+  const handleClick = () => {
+    lightValue(!light)
+  }
+
+  
+
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="App" style={{backgroundColor: "#51E1ED", width:width, height: height}}>
+
+      <span style={{position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)'}}>
+
+        {light ? <button style={{backgroundColor: 'orange'}} onClick={handleClick}>Turn light Off</button> : <button style={{backgroundColor: 'orange'}} onClick={handleClick}>Turn light On</button> }
+        
+        {light ? <p style={{color: '#5A20CB'}}>Light is Turned On right now </p> : <p style={{color: 'red'}}>Light is Turned Off right now </p>}
+
+        {light ? <HiLightBulb /> : <GoLightBulb /> }
+
+      </span>
     </div>
   );
 }
 
 export default App;
+
+
+
+  // button: {
+  //   backgroundColor: 'orange',
+  //   marginLeft: 30,
+  //   marginRight: 30,
+  //   marginTop: 20,
+  //   height: 58,
+  //   borderRadius: 200,
+  //   alignItems: "center",
+  //   justifyContent: 'center',
+  //   flexDirection: "row"
+  // }
